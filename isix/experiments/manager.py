5a65dcb9c6f7860e95c732bb8143013d5d3f305c
Matt <mattator@gmail.com>
2013-10-22 13:47:19 +0200

    Many changes

--- isix/experiments/manager.py
+++ isix/experiments/manager.py
@@ -1,5 +1,44 @@
 
+import sqlite3 as db
 
+
+def create_db(filename):
+
+    # check it does not exist
+
+    # 
+    # Create tables experiment/result/tcpstat
+        # mptcp INTEGER, 
+        # lisp INTEGER, 
+        # status INTEGER DEFAULT -1,
+    """
+    CREATE TABLE experiments (
+        name VARCHAR(50),
+        date INTEGER,
+        
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        );"""
+        # Ajouter les hotes
+
+    cur.executescript("""
+
+
+    CREATE TABLE results(
+        filesize INTEGER,
+        duration REAL,
+        experiment INTEGER,
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        );
+
+    CREATE TABLE tcp_stats(
+        min_rtt REAL DEFAULT 0,
+        max_rtt REAL DEFAULT 0,
+        avg_rtt REAL DEFAULT 0,
+        result_id INTEGER
+        )
+
+
+    """
 
 # should be able to detect failed tests,
 # propose the user to resume it or other options whenfacing a failure 
@@ -11,4 +50,5 @@ class XPManager:
 		# TODO load config
 		self._config = config
 
-	# def run_tests():
+	def run_tests():
+        
---
+++ probe/README.md
@@ -0,0 +1,1 @@
+Inpired by tcpprobe in order to retrieve data from MPTCP subflows without impacting the performance (contrary to a packet capture such as tcpdump or wireshark)
